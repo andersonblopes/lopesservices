d9568a8c6f613ee0693e83a392e8c2ffc4aa3a99
Anderson Lopes <anderson@lopessystem.com>
2023-02-13 21:12:49 +0100

    Configures eureka clients

--- fraud/src/main/java/com/lopessystem/fraud/FraudApplication.java
+++ fraud/src/main/java/com/lopessystem/fraud/FraudApplication.java
@@ -2,11 +2,13 @@ package com.lopessystem.fraud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * The type Fraud application.
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class FraudApplication {
 
     /**
