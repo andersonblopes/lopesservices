2a775bf36c768198a2931499c3b5bd1c04c16d3e
Anderson Lopes <anderson@lopessystem.com>
2023-02-21 18:50:25 +0100

    Sets messages configs

--- notification/src/main/java/com/lopessystem/notification/config/NotificationConfig.java
+++ notification/src/main/java/com/lopessystem/notification/config/NotificationConfig.java
@@ -1,7 +1,12 @@
 package com.lopessystem.notification.config;
 
 import lombok.Getter;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -28,5 +33,38 @@ public class NotificationConfig {
      */
     @Value("${rabbitmq.routing-keys.internal-notification}")
     private String internalNotificationRoutingKey;
+
+    /**
+     * Notification queue queue.
+     *
+     * @return the queue
+     */
+    @Bean
+    public Queue notificationQueue() {
+        return new Queue(notificationQueue);
+    }
+
+    /**
+     * Topic exchange topic exchange.
+     *
+     * @return the topic exchange
+     */
+    @Bean
+    public TopicExchange topicExchange() {
+        return new TopicExchange(internalExchange);
+    }
+
+    /**
+     * Internal to notification binding binding.
+     *
+     * @return the binding
+     */
+    @Bean
+    public Binding internalToNotificationBinding() {
+        return BindingBuilder
+                .bind(notificationQueue())
+                .to(topicExchange())
+                .with(internalNotificationRoutingKey);
+    }
 
 }
