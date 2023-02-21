4b5bda7bb750fe1428a6798d084273ea54708c61
Anderson Lopes <anderson@lopessystem.com>
2023-02-21 19:27:17 +0100

    Publishes a mensage on queue

--- notification/src/main/java/com/lopessystem/notification/NotificationApplication.java
+++ notification/src/main/java/com/lopessystem/notification/NotificationApplication.java
@@ -1,13 +1,22 @@
 package com.lopessystem.notification;
 
+import com.lopessystem.amqp.RabbitMQMessageProducer;
+import com.lopessystem.notification.config.NotificationConfig;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 /**
  * The type Notification application.
  */
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.lopessystem.notification",
+                "com.lopessystem.amqp"
+        }
+)
 @EnableEurekaClient
 public class NotificationApplication {
 
@@ -19,4 +28,16 @@ public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
     }
+
+    @Bean
+    CommandLineRunner runner(RabbitMQMessageProducer producer, NotificationConfig config) {
+        return args -> {
+            producer.publish(new Person("Anderson", 41), config.getInternalExchange(),
+                    config.getInternalNotificationRoutingKey());
+        };
+    }
+
+    record Person(String name, int age) {
+    }
+
 }
