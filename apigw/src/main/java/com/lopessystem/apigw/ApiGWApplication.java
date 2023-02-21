58bca68c295a98b67a0de0ddbc3cf93725a9a87d
Anderson Lopes <anderson@lopessystem.com>
2023-02-21 15:48:29 +0100

    Includes api gateway configs

--- apigw/src/main/java/com/lopessystem/apigw/ApiGWApplication.java
+++ apigw/src/main/java/com/lopessystem/apigw/ApiGWApplication.java
@@ -3,10 +3,12 @@ package com.lopessystem.apigw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * The type Api gw application.
  */
+@EnableEurekaClient
 @SpringBootApplication
 public class ApiGWApplication {
 
