64ce7fbcbf53cad6b551583b87182dce0de9d156
Anderson Lopes <anderson@lopessystem.com>
2023-02-21 15:40:24 +0100

    Includes api gateway module

---
+++ apigw/src/main/java/com/lopessystem/apigw/ApiGWApplication.java
@@ -0,0 +1,22 @@
+package com.lopessystem.apigw;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The type Api gw application.
+ */
+@SpringBootApplication
+public class ApiGWApplication {
+
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+
+        SpringApplication.run(ApiGWApplication.class, args);
+    }
+}
